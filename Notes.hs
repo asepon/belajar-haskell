7755415422a38d758e6585d005950af7586df5f8
asepon <ponto.andrew@gmail.com>
2022-03-01 00:23:17 +0700

    Add Pertemuan 2

--- Notes.hs
+++ Notes.hs
@@ -1,4 +1,3 @@
-import GHC (ApplicativeArg(xarg_app_arg_many))
 -- | Pertemuan 1
 
 --load file to ghci
@@ -123,7 +122,7 @@ import GHC (ApplicativeArg(xarg_app_arg_many))
 -- ghci> myCond 2
 -- 4
 
--- TUGAS
+-- TUGAS 1
 -- 1. berapa nilai x * x 
 -- ghci> let x = 4
 -- ghci> let y = x + 3 
@@ -187,3 +186,248 @@ import GHC (ApplicativeArg(xarg_app_arg_many))
 --                      else 
 --                         "lebih besar dari 6"
 
+-- | Pertemuan 2 
+-- cekNilai :: Int -> String 
+-- cekNilai n = if n < 0 then "kurang dari nol" else "lebih dari nol"
+-- >>> cekNilai 2
+-- "lebih dari nol"
+-- >>> cekNilai (-2)
+-- "kurang dari nol"
+
+-- cekNilai1 :: Int -> String 
+-- cekNilai1 n 
+--   | n < 0 = "Kurang dari nol"
+--   | otherwise = "lebih dari nol" 
+-- >>> cekNilai1 1
+-- >>> cekNilai1 (-1)
+-- "lebih dari nol"
+-- "Kurang dari nol"
+
+-- hailstone :: Integer -> Integer 
+-- hailstone n 
+--         | n `mod` 2 == 0 = n `div` 2 
+--         | otherwise = 3 * n + 1 
+-- >>> hailstone 10
+-- 5
+-- >>> hailstone 3
+-- 10
+
+-- caseReplaceIf n = 
+--     case (n < 0) of 
+--         True -> "kurang dari nol"
+--         False -> "lebih dari nol" 
+-- >>> caseReplaceIf 2
+-- >>> caseReplaceIf (-1)
+-- "lebih dari nol"
+-- "kurang dari nol"
+
+-- myPatMatching :: Int -> Int -> Int 
+-- myPatMatching 2 3 = 2 + 3
+-- myPatMatching 2 n = 2 + n 
+-- myPatMatching n 0 = 2 + 10
+-- >>> myPatMatching 2 3
+-- 5
+-- >>> myPatMatching 2 0
+-- 2
+-- >>> myPatMatching 4 0
+-- 12
+-- myPatMatching' :: Int -> Int -> Int 
+-- myPatMatching' _ n = n
+-- >>> myPatMatching' 20 100
+-- >>> myPatMatching' 2 100
+-- 100
+-- 100
+
+-- contohPM :: [Int] -> [Int] 
+-- contohPM [] = [] 
+-- contohPM x = if head x == 2 then tail x else x 
+-- >>> contohPM [1,2,3,4]
+-- >>> contohPM [2,3,4]
+-- [1,2,3,4]
+-- [3,4]
+
+-- factorial :: Integer -> Integer 
+-- factorial 0 = 1 
+-- factorial n = n * factorial (n-1) 
+-- >>> factorial 5
+-- 120
+
+-- isEmpty :: String -> Bool 
+-- isEmpty "" = True 
+-- isEmpty theString = False 
+-- >>> isEmpty ""
+-- True
+-- >>> isEmpty "wew"
+-- False
+
+-- myNetWorth :: (Int, String)
+-- myNetWorth = (20, "Rupees") 
+-- >>> myNetWorth
+-- (20,"Rupees")
+
+-- sumPair :: (Int, Int) -> Int 
+-- sumPair (a,b) = a + b 
+-- >>> sumPair (2,4)
+-- 6
+
+-- sum3Arg :: Int -> Int -> Int -> Int 
+-- sum3Arg x y z = x + y + z 
+-- >>> sum3Arg 2 3 4
+-- 9
+
+-- result :: Int 
+-- result = sum3Arg 2 3 4 
+-- >>> result
+-- 9
+
+-- oneToTen :: [Integer] 
+-- oneToTen = [1..10]
+-- >>> oneToTen
+-- [1,2,3,4,5,6,7,8,9,10]
+
+-- evenBetweenOneAndTen :: [Integer]
+-- evenBetweenOneAndTen = [2,4..100]
+-- >>> evenBetweenOneAndTen
+-- [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100]
+
+-- theForceIs :: [Char]
+-- theForceIs = ['S','T','R','O','N','G']
+-- >>> theForceIs
+-- "STRONG"
+
+-- theForceIs' :: String 
+-- theForceIs' = "Strong"
+-- >>> theForceIs'
+-- "Strong"
+
+--pengecekan list ada isi atau tidak
+-- >>> null []
+-- True
+
+-- >>> null [1,2,3]
+-- False
+
+--menambahkan data ke list di depan
+-- >>> 4 : [1,3]
+-- [4,1,3]
+
+--menambahkan data ke list di belakang 
+-- >>> [1,3] ++ [2]
+-- [1,3,2]
+
+--menambahkan lebih dari 1 data ke list di depan
+-- >>> 1: 2: []
+-- [1,2]
+
+-- menghitung panjang list 
+-- >>> length [1,2,3]
+-- 3
+
+-- mengambil data dari list 
+-- >>> take 2 [1,2,3,4,5]
+-- [1,2]
+
+-- >>> take 6 [1,2,3,4]
+-- [1,2,3,4]
+
+--membalik elemen dari list
+-- >>> reverse [1,2,3,4]
+-- [4,3,2,1]
+
+--mengambil head dari list 
+-- >>> head [1,2,3,4]
+-- 1
+
+--mengambil semua data kecil data terakhir di list
+-- >>> init [1,2,3,4]
+-- [1,2,3]
+
+--mengambil elemen dari list 
+-- >>> [11,12,13,14] !! 3
+-- 14
+
+--menghapus sejumlah data dari list 
+-- >>> drop 4 [1,2,3,4,5,6]
+-- [5,6]
+
+--mengambil data terakhir dari list 
+-- >>> last [1,23,4,2,6,4]
+-- 4
+
+--mengambil maximum dan minimum dari list 
+-- >>> maximum [1,23,4,2,6,4]
+-- 23
+
+-- >>> minimum [1,23,4,2,6,4]
+-- 1
+
+--menjumlahkan semua data di list
+-- >>> sum [1,2,4]
+-- 7
+
+--mengkalikan semua data di list 
+-- >>> product [1,2,5]
+-- 10
+
+-- TUGAS 2
+
+-- 1. Buat sebuah function yg akan menambahkan setiap inputan dari user ke dalam list, didepan list yg ada (listny bebas), contoh listnya adalah [1,2,3]
+--    2 argument, argumen 1 = list, argument 2 bilangan ditambahkan ke list 
+-- task1 [1,2,3] 10 = [10,1,2,3]
+-- jawaban 
+task1 :: [Int] -> Int -> [Int]
+task1 x y = y : x 
+-- >>> task1 [1,2,3] 10
+-- [10,1,2,3]
+
+--2. Buat sebuah function yg akan menambahkan setiap inputan dari user kedalam list dibelakang list yg ada
+-- task2 [1,2,3] 20 = [1,2,3,20]
+-- jawaban 
+task2 :: [Int] -> Int -> [Int] 
+task2 x y = x ++ [y]
+-- >>> task2 [1,2,3] 20
+-- [1,2,3,20]
+
+--3. Buat function yg mengambil 1 angka didepan sebuah list 
+-- task3 [1,2,3] = 1
+-- jawaban 
+task3 :: [Int] -> Int 
+task3 x = head x 
+-- >>> task3 [1,2,3]
+-- 1
+
+--4. Buat sebuah function yg menggunakan guards dan mengambil 5 elemen list yg diberikan dalam argument,
+-- hanya jika panjang list lebih dari 10, jika kurang dari 10 maka biarkan saja 
+-- cekAmbil10 [1,2,3,4,5,6,7,8,9]
+-- [1,2,3,4,5,6,7,8,9]
+-- cekAmbil10 [1,2,3,4,5,6,7,8,9,10,11]
+-- [1,2,3,4,5]
+-- jawaban 
+-- cekAmbil10 :: [Int] -> [Int]
+-- cekAmbil10 x 
+--     | length x > 10 = take 5 x 
+--     | otherwise = x 
+-- >>> cekAmbil10 [1,2,3,4,5,6,7,8,9]
+-- [1,2,3,4,5,6,7,8,9]
+-- >>> cekAmbil10 [1,2,3,4,5,6,7,8,9,10,11]
+-- [1,2,3,4,5]
+
+-- 5. Buat function seperti tugas nomor 4 tapi dengan logic pattern matching dan ada basic type jika list nya kosong [] maka keluarkan kosong
+-- cekAmbil10PM [1,2,3,4,5,6,7,8,9,10,12]
+-- [1,2,3,4,5]
+-- cekAmbil10PM [1,2,3,4,5,6,7,8,9,10]
+-- [1,2,3,4,5,6,7,8,9,10]
+-- cekAmbil10PM []
+-- [] 
+-- jawaban 
+-- cekAmbil10PM :: [Int] -> [Int]
+-- cekAmbil10PM [] = []
+-- cekAmbil10PM x 
+--     | length x > 10 = take 5 x 
+--     | otherwise = x
+-- >>> cekAmbil10PM [1,2,3,4,5,6,7,8,9,10,12]
+-- [1,2,3,4,5]
+-- >>> cekAmbil10PM [1,2,3,4,5,6,7,8,9,10]
+-- [1,2,3,4,5,6,7,8,9,10]
+-- >>> cekAmbil10PM []
+-- []
